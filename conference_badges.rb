954ec91f3f6ac55f978ec5d15a21bf8d02daccc6
Allen70 <Allenlea2007@gmail.com>
2020-10-28 08:15:58 -0600

    commit tree a2b0a9a25947dc42b9e4f3108c7696867f3f5484

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+require 'pry'
+def badge_maker name
+     "Hello, my name is #{name}."
+end
+
+def batch_badge_creator list
+list.map {|attendees| "Hello, my name is #{attendees}."}
+end
+
+def assign_rooms list
+    list.map {|attendees| "Hello, #{attendees}! You'll be assigned to room #{list.find_index("#{attendees}")+1}!" }
+end
+
+def printer list
+    list.map {|attendee| puts "Hello, my name is #{attendee}."}
+    list.map {|attendee| puts "Hello, #{attendee}! You'll be assigned to room #{list.find_index("#{attendee}")+1}!"}
+end
